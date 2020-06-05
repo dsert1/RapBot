65c1585cc30fbc4515bf28294fc4c89a34b6268d
Deniz Sert <dsert@mit.edu>
2020-06-04 19:24:39 -0700

    scrape.py --> {word: rhyming_words}
    Added mapping of word to set of rhyming words

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 from urllib.request import urlopen as ureq
 from urllib import request
 import urllib
-
+import pronouncing
 from bs4 import BeautifulSoup as soup
 import certifi
 import requests
+import prettyprinter
 
 
 def get_words():
@@ -34,8 +35,7 @@ def write_to_file(word):
     no return'''
     filename = 'words.txt'
     with open(filename, 'a+') as f:
-        f.write('\n')
-        f.write(word)
+        f.write('\n' + '\'' + word + '\'' + ',')
         f.close()
 
 def write_all_to_file(word_set):
@@ -43,9 +43,45 @@ def write_all_to_file(word_set):
     for word in word_set:
         write_to_file(word)
 
+def text_file_to_set():
+    '''reads the contents off a text file and
+    turns it into a set of words
+
+    returns a set
+    '''
+    word_set = set()
+    filename = 'words.txt'
+    with open(filename, 'r') as f:
+        words = f.read()
+    words = words.replace(',\n', ' ')
+    for word in words.split():
+        stripped_word = word.strip('\'')
+        word_set.add(stripped_word)
+    return word_set
+
+
+def create_rhyme_set(word):
+    '''creates a set of rhyming words off the input word'''
+
+    with open('resources/words2.txt') as f:
+        allwords = set(f.read().splitlines())
+
+    def is_word(x):
+        return x in allwords
+
+    return {word for word in pronouncing.rhymes(word) if is_word(word)}
+
+def rhyme_words():
+    '''
+    returns a dictionary - word: {rhyming_words}
+    '''
+    return {word: create_rhyme_set(word) for word in text_file_to_set() if create_rhyme_set(word)}
+
+
 
 if __name__ == '__main__':
     # write_to_file('work')
     # print(get_words())
 
-    write_all_to_file(get_words())
+    # write_all_to_file(get_words())
+    print(rhyme_words())
